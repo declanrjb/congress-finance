7ffe17625a63eabb1040700fedb5579f1f8568f4
declanrjb <declanrjb@gmail.com>
2022-11-16 13:57:28 -0800

    Building congressional graph

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { parse } from "graphology-gexf/browser";
 import { Coordinates, EdgeDisplayData, NodeDisplayData } from "sigma/types";
 
 // Load external GEXF file:
-fetch("./senate2022v3.gexf")
+fetch("./congress2022v8.gexf")
   .then((res) => res.text())
   .then((gexf) => {
     // Parse GEXF string:
